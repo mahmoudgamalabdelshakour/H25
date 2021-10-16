1d085d87f08a8ebff629f401e3250e7021319022
mahmoudgamalabdelshakour <shoky641@gmail.com>
2021-10-16 10:06:04 +0200

    note

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+// this is a single line comment
+//include standard input output to use printf function
+/*my entry point to start the program 
+this is the main implementation
+int =>return type
+void =>no thing
+*/
+#include <stdio.h>
+int main(void){
+	printf("\"Hello world\n in c\"");
+return 0 ;
+}
